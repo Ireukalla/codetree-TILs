667fb555c915a327164b4ab64942804377bcdc4c
Ireukalla <162826159+Ireukalla@users.noreply.github.com>
2024-04-02 23:38:33 +0900

    Title: 007, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240402/007/007.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+const input = () => {
+  const input = fs.readFileSync('/dev/stdin').toString()
+  const tokens = input.split('\n').flatMap((token) => token.split(' '))
+  const validTokens = tokens.filter((token) => token !== '')
+
+  return validTokens
+}
+
+const TOKENS = input()
+let TOKEN_INDEX = 0
+
+const scanString = () => {
+  const stringValue = TOKENS[TOKEN_INDEX]
+  TOKEN_INDEX += 1
+  return stringValue
+}
+
+const scanInt = () => {
+  const intValue = parseInt(TOKENS[TOKEN_INDEX])
+  TOKEN_INDEX += 1
+  return intValue
+}
+
+// -------------------------------------------- CODE -------------------------------------------- //
+
+let secretCode = scanString()
+let location = scanString()
+let time = scanString()
+
+console.log(`secret code : ${secretCode}`)
+console.log(`meeting point : ${location}`)
+console.log(`time : ${time}`)
